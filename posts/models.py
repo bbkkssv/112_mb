6ae5daad1d26907c4b60e483685f17b3baee2beb
Robert Vinson <bbkkssv@gmail.com>
2026-02-17 16:02:13 -0600

    assignment 3

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model): #OOP - Classes and Objects
+    title = models.CharField(max_length=128)
+    subtitle = models.CharField(max_length=256)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+
+    # dunder methods or double underscor methods or magic methods in python
+    def __str__(self): #or also called the "toString" method
+        return f"{self.title} - {self.author}"
+    
+   
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[self.id])
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DeleteView,
+    DetailView,
+    CreateView,
+    UpdateView
+)
+from .models import Post
 
 # Create your views here.
+class PostListView(ListView): #GET Request -> List
+    # template_name attribute renders a specific html file
+    template_name = "posts/list.html"
+    # model attribute let django know from which model (table) we want to retrieve the data
+    model = Post
+    # context_object_name attribute allow us to change the variable on how we call it inside of the templates
+    context_object_name = "posts"
